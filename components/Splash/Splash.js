454a5679e1a515ef7989dfc001ad24a8c99a7509
sternj20 <jessesternmusic@gmail.com>
2018-02-04 11:23:47 -0800

    added img to splash page

--- components/Splash/Splash.js
+++ components/Splash/Splash.js
@@ -10,7 +10,7 @@ const Splash = props => {
             </div>
             <div className="row">
                 <div className="imgs">
-                 IMGS GO HERE                                               
+                 <img src="../../assets/phil-coffman-161251.jpg" className="splashImg"/>                                               
                 </div>
             </div>
             <div className="row">            
@@ -21,16 +21,18 @@ const Splash = props => {
                 
 
 
-                <div className="login col-md-12">
-                    <div>                
+                <div className="row">
+                <div className="col-md-4">
                         <label><b>User Name</b></label>
                         <input className="userName" placeholder="Enter User Name" name="psw" required/>
                     </div>
-                    <div>                
+                    <div className="col-md-4">                
                         <label><b>Password</b></label>
                         <input type="password" className="pw" placeholder="Enter Password" name="psw" required/>
+                        </div>
+                        <div className="col-md-4">
+                        <button className="btn-lg footer" onClick={props.login}>Login</button>
                     </div>
-                        <button className="btn-lg demoBtn col-md-6 footer" onClick={props.login}>Login</button>
                     </div>
                 </div>
          </div>
