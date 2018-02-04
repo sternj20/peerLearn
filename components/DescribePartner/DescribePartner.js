25a22a7569167d0980834a9da7cee19ea46192d8
hanguyen0712 <36115270+hanguyen0712@users.noreply.github.com>
2018-02-03 18:55:22 -0800

    basic learning options wireframe

---
+++ components/DescribePartner/DescribePartner.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const DescribePartner = () => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="header"><h1>Describe Partner</h1></div>
+            </div>
+            <div className="optionRowContainer">
+                <div className="row">
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>                
+                </div>
+                <div className="row">            
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>                
+                </div>
+            </div>
+
+            <div className="optionRowContainer">
+                <div className="row">
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>                
+                </div>
+                <div className="row">            
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>                
+                </div>
+            </div>
+
+            <div className="optionRowContainer">
+                <div className="row">
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>                
+                </div>
+                <div className="row">            
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>
+                    <btn className="btn-lg col">Quality</btn>                
+                </div>
+            </div>
+            <div className="row">            
+                <btn className="btn-lg col-sm-10">Next</btn>
+            </div>
+        </div>
+    )
+}
+
+export default DescribePartner;
+
